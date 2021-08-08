d766fe15634b62225ae72b2b22416a7608ccd94c
Damien Vesper <ldamienvesper@gmail.com>
2021-08-08 09:52:19 -0500

    cf recaptcha moment

--- src/api/api.py
+++ src/api/api.py
@@ -5,18 +5,23 @@ import json as JSON
 
 # Get arguments.
 parser = argparse.ArgumentParser()
-parser.add_argument("method")
-parser.add_argument("url")
-parser.add_argument("data")
+parser.add_argument("--method")
+parser.add_argument("--url")
+parser.add_argument("--data")
 args = parser.parse_args()
 
 # Initialize the scraper.
-scraper = cloudscraper.create_scraper()
+scraper = cloudscraper.create_scraper(
+    interpreter = "nodejs",
+    captcha = {
+        "provider": "return_response"
+    }
+)
 
-if (args.method == "get"):
+if (args.method == "GET"):
     # Send a GET request to the endpoint.
     print(scraper.get(args.url).text)
-elif (args.method == "post"):
+elif (args.method == "POST"):
     # Send a POST request to the endpoint with an optional payload
     print(scraper.post(args.url, JSON.loads(args.data) if args.data else {}).text)
 else:
