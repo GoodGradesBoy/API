bfaab41e4ebf6a23c352084f4ead2f86dc3a929b
Damien Vesper <ldamienvesper@gmail.com>
2021-09-02 10:01:07 -0500

    refactor: package upgrades, drop python

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,27 +1,7 @@
-import { spawn } from 'child_process';
-import { platform } from 'process';
-import { resolve } from 'path';
+import axios from 'axios';
 
 const baseURL = `https://nitrotype.com/api`;
-const pyBin = platform === `win32` ? `py` : `python`;
 
-class APIClient {
-    get = (uri: string): unknown => {
-        return spawn(pyBin, [
-            resolve(__dirname, `./api.py`),
-            `--method GET`,
-            `--url ${baseURL + uri}`
-        ]);
-    }
-
-    post = (uri: string, data: unknown): unknown => {
-        return spawn(pyBin, [
-            resolve(__dirname, `./api.py`),
-            `--method POST`,
-            `--url ${baseURL + uri}`,
-            `--data ${data ? JSON.stringify(data) : JSON.stringify({})}`
-        ]);
-    }
-}
+class APIClient {}
 
 export default APIClient;
